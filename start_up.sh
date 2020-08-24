f86465b2c6864ab571649253cc246690f3802343
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-08-24 15:27:57 -0400

    update docker-compose

--- start_up.sh
+++ start_up.sh
@@ -2,6 +2,8 @@ sudo apt-get update
 
 sudo apt-get install docker-compose -y
 
+export IP=`curl http://checkip.amazonaws.com`
+
 git clone https://github.com/jweissenberger/outpost-api.git
 
 cd outpost-api
