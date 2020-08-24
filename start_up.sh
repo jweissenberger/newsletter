298bd4f7f9631ac36bd5f7e11de6e03e6a1194de
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-08-24 14:40:06 -0400

    update timeout

--- start_up.sh
+++ start_up.sh
@@ -1,3 +1,11 @@
 sudo apt-get update
 
 sudo apt-get install docker-compose -y
+
+git clone https://github.com/jweissenberger/outpost-api.git
+
+cd outpost-api
+
+sudo docker-compose build
+
+sudo docker-compose up
