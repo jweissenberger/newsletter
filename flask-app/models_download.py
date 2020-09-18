d97111657911f7c90be4e1851e263406173065e9
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-09-18 17:11:13 -0400

    add back nltk download because it will cause an error with t5

--- flask-app/models_download.py
+++ flask-app/models_download.py
@@ -1,12 +1,12 @@
 from transformers import T5ForConditionalGeneration, PegasusForConditionalGeneration, PegasusTokenizer
-# import nltk
+import nltk
 
 if __name__ == '__main__':
     #nltk
-    # nltk.download('vader_lexicon')
-    # nltk.download('subjectivity')
-    # nltk.download('punkt')
-    # nltk.download('stopwords')
+    nltk.download('vader_lexicon')
+    nltk.download('subjectivity')
+    nltk.download('punkt')
+    nltk.download('stopwords')
 
     # T5 model download
     model = T5ForConditionalGeneration.from_pretrained('t5-large')
