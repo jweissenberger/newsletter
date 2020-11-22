7a89850366574ac58e5ab67bb4a9d26870ff9825
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-11-22 13:39:31 -0800

    update requirements

--- flask-app/models_download.py
+++ flask-app/models_download.py
@@ -1,18 +1,9 @@
-from transformers import T5ForConditionalGeneration, PegasusForConditionalGeneration, PegasusTokenizer
-import nltk
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
 if __name__ == '__main__':
-    #nltk
-    nltk.download('vader_lexicon')
-    nltk.download('subjectivity')
-    nltk.download('punkt')
-    nltk.download('stopwords')
-
-    # T5 model download
-    model = T5ForConditionalGeneration.from_pretrained('t5-large')
 
     # pegasus download
-    models = ['google/pegasus-xsum', 'google/pegasus-multi_news']
+    models = ['google/pegasus-cnn_dailymail']
     for model in models:
         tokenizer = PegasusTokenizer.from_pretrained(model)
         pegasus = PegasusForConditionalGeneration.from_pretrained(model)
