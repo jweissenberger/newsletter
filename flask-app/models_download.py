40586939ee19ec81d33da6f655bb72c08eecaa78
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-11-22 13:53:45 -0800

    update model downloads

--- flask-app/models_download.py
+++ flask-app/models_download.py
@@ -1,6 +1,11 @@
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
 if __name__ == '__main__':
+    #nltk
+    nltk.download('vader_lexicon')
+    nltk.download('subjectivity')
+    nltk.download('punkt')
+    nltk.download('stopwords')
 
     # pegasus download
     models = ['google/pegasus-cnn_dailymail']
